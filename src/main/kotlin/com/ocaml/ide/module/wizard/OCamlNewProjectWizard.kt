8c9e3ef920bc1ec75a363c4e03a61d0d2d7413be
Quentin RA <quentin.ramsamy.cf@gmail.com>
2024-06-19 19:01:02 +0200

    Testing Build Systems

--- src/main/kotlin/com/ocaml/ide/module/wizard/OCamlNewProjectWizard.kt
+++ src/main/kotlin/com/ocaml/ide/module/wizard/OCamlNewProjectWizard.kt
@@ -1,20 +1,15 @@
 package com.ocaml.ide.module.wizard
 
-import com.intellij.ide.projectWizard.NewProjectWizardConstants
 import com.intellij.ide.wizard.AbstractNewProjectWizardMultiStep
 import com.intellij.ide.wizard.AbstractNewProjectWizardStep
-import com.intellij.ide.wizard.BuildSystemNewProjectWizardData
-import com.intellij.ide.wizard.LanguageNewProjectWizardData.Companion.languageData
 import com.intellij.ide.wizard.NewProjectWizardStep
 import com.intellij.ide.wizard.language.LanguageGeneratorNewProjectWizard
-import com.intellij.openapi.observable.properties.GraphProperty
-import com.intellij.openapi.project.Project
 import com.ocaml.OCamlBundle.message
 import com.ocaml.icons.OCamlIcons
 import com.ocaml.ide.module.wizard.buildSystem.BuildSystemOCamlNewProjectWizard
 import javax.swing.Icon
 
-interface BuildSystemOCamlNewProjectWizardData : BuildSystemNewProjectWizardData
+// I removed "BuildSystemNewProjectWizardData" as I don't know it's use
 
 // For now, we can't build anything
 // But, assume we have a "Makefile" build system, a "dune" build system, etc.
@@ -34,37 +29,9 @@ class OCamlNewProjectWizard : LanguageGeneratorNewProjectWizard {
     override fun createStep(parent: NewProjectWizardStep): NewProjectWizardStep = OCamlNewProjectWizardStep(parent)
 
     class OCamlNewProjectWizardStep(parent: NewProjectWizardStep) :
-        AbstractNewProjectWizardMultiStep<OCamlNewProjectWizardStep, BuildSystemOCamlNewProjectWizard>(parent, BuildSystemOCamlNewProjectWizard.EP_NAME),
-        BuildSystemOCamlNewProjectWizardData
+        AbstractNewProjectWizardMultiStep<OCamlNewProjectWizardStep, BuildSystemOCamlNewProjectWizard>(parent, BuildSystemOCamlNewProjectWizard.EP_NAME)
     {
         override val label: String  get() = message("project.wizard.build.system")
         override val self: OCamlNewProjectWizardStep get() = this
-
-        override fun setupProject(project: Project) {}
-
-        override var buildSystem: String
-            get() = step
-            set(value) { step = value }
-        override val buildSystemProperty: GraphProperty<String>
-            get() = stepProperty
-
-        @Deprecated("See LanguageNewProjectWizardData documentation for details")
-        override var language: String
-            get() = languageData!!.language
-            set(value) { languageData!!.language = value }
-        override val languageProperty: GraphProperty<String>
-            get() = languageData!!.languageProperty
-
-        override var name: String
-            get() = languageData!!.name
-            set(value) { languageData!!.name = value }
-        override val nameProperty: GraphProperty<String>
-            get() = languageData!!.nameProperty
-
-        override var path: String
-            get() = languageData!!.path
-            set(value) { languageData!!.path = value }
-        override val pathProperty: GraphProperty<String>
-            get() = languageData!!.pathProperty
     }
 }
